fcc0ad954c2f0055736e1f6323e61e9254c260ae
Michael Trollan <MichaelTrollan@Michael-Trollans-MacBook-Air.local>
2012-06-08 18:49:05 -0400

    hi

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyCritics2::Application.config.secret_token = 'c8161c1472c491fd74a6d0027caf4ee39e369780d4747c3714a5da182120aad27cfb32c82aa6fa1dfcfd6a4db0e20cc357d5bbe19bf5c80e2b5fda4c3e46fd3e'
